a1161782c9b8f7f51e80dfbb4cfff47133c9e57d
Nandakishore Saboo <nandakishoresaboo@gmail.com>
2017-11-30 20:24:08 +0530

    - adds client skeleton code structure

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, 'client'),
+  entry: './index.jsx',
+  output: {
+    path: path.join(__dirname, 'public/dist'),
+    filename: 'prototype-pe.bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      }
+    ]
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, 'node_modules'),
+    ],
+    extensions: ['.js', '.jsx'],
+  },
+};
