19ceba319f84be7dbcb9d5af71c157d4487de97b
Nandakishore Saboo <nandakishoresaboo@gmail.com>
2017-12-02 15:45:14 +0530

    adds node server access log for logstash consumption

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 const path = require('path');
 const express = require('express');
+const fs = require('fs');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const router = require('./routes');
 
 const PORT = process.env.PORT || 3000;
+const EXPRESS_LOG_FILE = process.env.EXPRESS_LOG_FILE || './access.log';
+const accessLogStream = fs.createWriteStream(`${EXPRESS_LOG_FILE}`, { flags: 'a' });
 
 const app = express();
 
-app.use(morgan());
+app.use(morgan('combined', { stream: accessLogStream }));
 app.use(express.static(path.join(__dirname, '../public/')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
